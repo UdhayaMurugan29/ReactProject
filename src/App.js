1138294f4d66aaca8b6ffb0408b845a1f214ecd5
Your Name <you@example.com>
2024-03-04 15:03:44 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Loginform from './Components/Loginform/Loginform';
+
+function App() {
+  return (
+    <div>
+      <Loginform />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Loginform/Loginform.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Loginform.css';
+import logo from './img/Group 3.png';
+
+const Loginform = () => {
+  return (
+    
+    <div className="login-form">
+    
+    <form>
+      
+      <label><img src={logo} alt="Logo" className="logo" /></label>
+      <label class = 'text'> We are electric </label>
+      <label htmlFor="username"> </label>
+      <input type="text" id="username" name="username" placeholder="Email" />
+      <label htmlFor="password"> </label>
+      <input type="password" id="password" name="password" placeholder="Password" />
+      <label></label><button type="submit">Login</button><label></label>
+      <label class="forgot-password">Forgot Password?</label>
+     
+    </form></div>
+  )
+}
+
+export default Loginform
