bbb92d9b0851303d6f359ab7cd6bdd3f086fd8d2
Your Name <you@example.com>
2024-03-04 19:23:45 +0530

    Testing

---
+++ public/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './LoginForm';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <LoginForm />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
