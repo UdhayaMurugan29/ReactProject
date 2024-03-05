c726e0281ba6e58a450c6ed2da99298329844446
Your Name <you@example.com>
2024-03-05 14:46:46 +0530

    Testing

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Loginform from './Components/Loginform/Loginform';
-import Dashboard from './Components/Dashboard/Dashboard';
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Loginform />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </Router>
-
-  );
-}
-
-export default App;
-
 // import React from 'react';
-// import Login from './Components/Loginform/Loginform'; // Assuming Login is your login component
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Loginform from './Components/Loginform/Loginform';
+// import Dashboard from './Components/Dashboard/Dashboard';
 
 // function App() {
 //   return (
-//     <div>
-//       <Login />
-//     </div>
+//     <Router>
+//       <Routes>
+//         <Route path="/" element={<Loginform />} />
+//         <Route path="/dashboard" element={<Dashboard />} />
+//       </Routes>
+//     </Router>
+
 //   );
 // }
 
 // export default App;
+
+import React from 'react';
+import Login from './Components/Loginform/Loginform'; // Assuming Login is your login component
+
+function App() {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+}
+
+export default App;
